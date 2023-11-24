b37132605b79adb5fe2101567782d5ae1d152117
Budiul Cristian-Carol <cristib_2002@yahoo.com>
2023-11-24 19:02:37 +0200

    Initial commit, essential fixes included

--- postbuild/paths.cmd
+++ postbuild/paths.cmd
@@ -12,6 +12,7 @@ set PATH_TO_VM_DISK=__EMPTY__
 set PATH_TO_VM_TOOLS=__EMPTY__
 set VOL_MOUNT_LETTER=__EMPTY__
 
+if _%COMPUTERNAME%_==_DESKTOP-NITRO5_ goto config_DESKTOP-NITRO5
 if _%COMPUTERNAME%_==_AGURZOU-DEV-PC_ goto config_AGURZOU-DEV
 if _%COMPUTERNAME%_==_ALEX-PC_ goto config_ALEX-PC
 if _%COMPUTERNAME%_==_AGURZOU-LPT_ goto config_AGURZOU-LPT
@@ -67,6 +68,17 @@ set PXE_PATH2=pxe-GRZ
 
 set BOCHS_PATH="c:\Program Files (x86)\Bochs-2.6.8\"
 
+goto end
+
+:config_DESKTOP-NITRO5
+
+SET PATH_TO_VM_FILE="D:\OneDrive - Technical University of Cluj-Napoca\UTCN\An IV\S1\_Cod_Proiecte_Teme\PSO_TestP\HAL9000\VM\HAL9000_VM\HAL9000.vmx"
+SET PATH_TO_VM_TOOLS="C:\Program Files (x86)\VMware\VMware Virtual Disk Development Kit"
+SET PXE_PATH="D:\OneDrive - Technical University of Cluj-Napoca\UTCN\An IV\S1\_Cod_Proiecte_Teme\PSO_TestP\HAL9000\PXE"
+SET PATH_TO_LOG_FILE="D:\OneDrive - Technical University of Cluj-Napoca\UTCN\An IV\S1\_Cod_Proiecte_Teme\PSO_TestP\HAL9000\VM\HAL9000_VM\HAL9000.log"
+SET PATH_TO_VIX_TOOLS="C:\Program Files (x86)\VMware\VMware Workstation"
+SET PATH_TO_VM_DISK="D:\OneDrive - Technical University of Cluj-Napoca\UTCN\An IV\S1\_Cod_Proiecte_Teme\PSO_TestP\HAL9000\VM\HAL9000_VM\HAL9000.vmdk"
+SET VOL_MOUNT_LETTER="Q:"
 goto end
 
 :end
